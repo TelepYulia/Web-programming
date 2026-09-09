2b054a697bfd013a26ac9a2358e7ee1f7d300a0d
TelepYulia <telep.yuliia@comp-sc.if.ua>
2026-09-09 11:52:38 +0300

    Update lab1.js

--- lab1/lab1.js
+++ lab1/lab1.js
@@ -1,1 +1,10 @@
+function triangle (value1, type1, value2, type2) {
+    let leg, hypotenuse, adjacent_angle, opposide_angle, angle;
+    let a,b,c, alpha, beta;
 
+    if((type1 === "leg") && (type2 === "leg")){
+        
+
+    }
+    console.log(value1, type1, value2, type2);
+}
